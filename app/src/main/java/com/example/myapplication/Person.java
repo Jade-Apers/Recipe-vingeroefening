07f289874f69d001cafd158f78d196926cf78f4a
Jade Apers <jadeapers@telenet.be>
2022-03-07 09:24:03 +0100

    add class

--- app/src/main/java/com/example/myapplication/Person.java
+++ app/src/main/java/com/example/myapplication/Person.java
@@ -3,13 +3,28 @@ package com.example.myapplication;
 public class Person {
 
     int amount;
+    int ingredient;
+    int amountCount;
 
-    Person (amount) {
+    public int getAmount() {
+        return amount;
+    }
+    public int getIngredient(){
+        return ingredient;
+    }
+
+    public int getAmountCount(){
+        return amountCount;
+    }
+
+    public void setAmount(int amount, int ingredient, int amountCount) {
         this.amount = amount;
+        this.ingredient = ingredient;
     }
 
-    public String setAmount() {
-        return this.amount;
+    public int amountCount(int amount, int ingredient) {
+        return amount * ingredient;
     }
-
 }
+
+
